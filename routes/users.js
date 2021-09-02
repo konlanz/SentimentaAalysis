703f0540f10fbb48f836d70ab78db352fd035ac9
konlan mikpekoah <konlanmikpekoah.km@outlook.com>
2021-09-02 02:34:44 +0000

    this  is cool

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,30 @@
 var express = require('express');
-var router = express.Router();
+const aposToLexForm = require('apos-to-lex-form');
 const natural = require('natural');
+const SpellCorrector = require('spelling-corrector');
+var router = express.Router();
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+const spellCorrector = new SpellCorrector();
+spellCorrector.loadDictionary();
+
 router.post('/anylizer', (req, res)=>{
   const {data} = req.body;
+  const lexedReview = aposToLexForm(review);
+  const casedReview = lexedReview.toLowerCase();
+  const alphaOnlyReview = casedReview.replace(/[^a-zA-Z\s]+/g, '');
+  
+  const { WordTokenizer } = natural;
+  const tokenizer = new WordTokenizer();
+  const tokenizedReview = tokenizer.tokenize(alphaOnlyReview);
+  tokenizedReview.forEach((word, index) => {
+    tokenizedReview[index] = spellCorrector.correct(word);
+  })
 })
 
 module.exports = router;
